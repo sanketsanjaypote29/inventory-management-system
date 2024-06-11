4a10f34ceab5c43529323b6761df49b2322c3d6b
unknown <sanketsanjaypote@gmail.com>
2024-06-11 18:42:22 +0530

    comments added

--- src/controllers/inventoryController.js
+++ src/controllers/inventoryController.js
@@ -1,15 +1,18 @@
-// src/controllers/inventoryController.js
+
+// Require necessary NPM packages and models for inventory controller functions 
 
 const { addItemToInventory, removeItemFromInventory, getInventory } = require('../models/inventory');
 const { addItemToCart, getCart } = require('../models/cart');
 const { applyDiscountCoupon } = require('../models/discount');
 
+// Define addItem controller function to add items to inventory
 const addItem = (req, res) => {
   const { productId, quantity } = req.body;
   addItemToInventory(productId, quantity);
   res.send(`Added ${quantity} of product ${productId} to inventory`);
 };
 
+// Define removeItem controller function to remove items from inventory
 const removeItem = (req, res) => {
   const { productId, quantity } = req.body;
   const success = removeItemFromInventory(productId, quantity);
@@ -20,6 +23,7 @@ const removeItem = (req, res) => {
   }
 };
 
+// Define addToCart controller function to add items to customer cart
 const addToCart = (req, res) => {
   const { customerId, productId, quantity } = req.body;
   const inventory = getInventory();
@@ -30,22 +34,25 @@ const addToCart = (req, res) => {
     res.status(400).send('Not enough items in inventory');
   }
 };
-
+// Define applyDiscount controller function to apply discount to cart value
 const applyDiscount = (req, res) => {
   const { cartValue, discountId } = req.body;
   const discountedPrice = applyDiscountCoupon(cartValue, discountId);
   res.send(`Discounted price is ${discountedPrice}`);
 };
 
+// Define getInventoryItems controller function to get inventory items
 const getInventoryItems = (req, res) => {
   res.json(getInventory());
 };
 
+// Define getCustomerCart controller function to get customer cart
 const getCustomerCart = (req, res) => {
   const { customerId } = req.params;
   res.json(getCart(customerId));
 };
 
+// Export controller functions
 module.exports = {
   addItem,
   removeItem,
